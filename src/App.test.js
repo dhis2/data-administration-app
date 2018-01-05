98032ab8a82f6e5e959ba4a87d235917ac9071ef
Joao <joao.antunes@present-technologies.com>
2018-01-05 08:21:28 +0000

    Preparing for deploy

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+*/
 
+const { it } = global;
 it('renders without crashing', () => {
+    /*
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+    */
 });
-*/
+
--- src/configs/maintenance.conf.js
+++ src/configs/maintenance.conf.js
@@ -1,19 +1,64 @@
 const maintenanceCheckboxes = [
-    { key: 'clearAnalyticsTables', label: 'Clear analytics tables' },
-    { key: 'analyzeAnalyticsTables', label: 'Analyze analytics tables' },
-    { key: 'removeZeroDataValues', label: 'Remove zero data values' },
-    { key: 'permRemoveSoftDeletedValues', label: 'Permanently remove soft deleted data values' },
-    { key: 'permRemoveSofDeletedEvents', label: 'Permanently remove soft deleted events' },
-    { key: 'permRemoveSoftDeletedEnrollments', label: 'Permanently remove soft deleted enrollments' },
-    { key: 'permRemoveSoftDeletedTrackedEntityInstances', label: 'Permanently remove soft deleted tracked entity instances' },
-    { key: 'prunePeriods', label: 'Prune periods' },
-    { key: 'removeExpiredInvitations', label: 'Remove expired invitations' },
-    { key: 'dropSqlViews', label: 'Drop SQL views' },
-    { key: 'createSqlViews', label: 'Create SQL views' },
-    { key: 'updateCategoryOptionsCombinations', label: 'Update category option combinations' },
-    { key: 'updateOrganisationUnitPaths', label: 'Update organisation unit paths' },
-    { key: 'clearApplicationCache', label: 'Clear application cache' },
-    { key: 'reloadApps', label: 'Reload apps' },
+    {
+        key: 'clearAnalyticsTables',
+        label: 'Clear analytics tables',
+    },
+    {
+        key: 'analyzeAnalyticsTables',
+        label: 'Analyze analytics tables',
+    },
+    {
+        key: 'removeZeroDataValues',
+        label: 'Remove zero data values',
+    },
+    {
+        key: 'permRemoveSoftDeletedValues',
+        label: 'Permanently remove soft deleted data values',
+    },
+    {
+        key: 'permRemoveSofDeletedEvents',
+        label: 'Permanently remove soft deleted events',
+    },
+    {
+        key: 'permRemoveSoftDeletedEnrollments',
+        label: 'Permanently remove soft deleted enrollments',
+    },
+    {
+        key: 'permRemoveSoftDeletedTrackedEntityInstances',
+        label: 'Permanently remove soft deleted tracked entity instances',
+    },
+    {
+        key: 'prunePeriods',
+        label: 'Prune periods',
+    },
+    {
+        key: 'removeExpiredInvitations',
+        label: 'Remove expired invitations',
+    },
+    {
+        key: 'dropSqlViews',
+        label: 'Drop SQL views',
+    },
+    {
+        key: 'createSqlViews',
+        label: 'Create SQL views',
+    },
+    {
+        key: 'updateCategoryOptionsCombinations',
+        label: 'Update category option combinations',
+    },
+    {
+        key: 'updateOrganisationUnitPaths',
+        label: 'Update organisation unit paths',
+    },
+    {
+        key: 'clearApplicationCache',
+        label: 'Clear application cache',
+    },
+    {
+        key: 'reloadApps',
+        label: 'Reload apps',
+    },
 ];
 
 export default maintenanceCheckboxes;
