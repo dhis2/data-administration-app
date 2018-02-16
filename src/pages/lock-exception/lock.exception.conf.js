38759dbe46eacd5bfb006d8fc9992b6db41b9b2b
MDHomem <mdhomem@gmail.com>
2018-02-16 16:29:50 +0000

    Add app config files

---
+++ src/pages/lock-exception/lock.exception.conf.js
@@ -0,0 +1,9 @@
+export const PAGE_TITLE = 'Lock Exceptions';
+
+export const PAGE_SUMMARY = 'Lock exceptions provide fine-grained control over exemption from a locked data set.' +
+    ' After the expiry of the data set, data entry will be denied by default, unless an exception has been granted' +
+    ' through the Lock exception interface. To enable a lock exception, select the desired organization units, data' +
+    ' sets, and time period and press "Add". By granting a lock exception, data entry will be enabled even after the' +
+    ' expiry period of the data set has passed. In the example above, a data lock exception would be created for "ab' +
+    ' Abundant Life Organization" and "ab Seventh Day Hospital" for the "Care and Support" dataset for "February' +
+    ' 2012".';
