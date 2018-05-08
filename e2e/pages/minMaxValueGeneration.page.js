f76fd25c75802c0630eee89c9e1c7a19425d6b4b
MDHomem <mdhomem@gmail.com>
2018-05-08 09:12:28 +0100

    Min-Max E2E tests implementation

--- e2e/pages/minMaxValueGeneration.page.js
+++ e2e/pages/minMaxValueGeneration.page.js
@@ -11,6 +11,22 @@ class MinMaxValueGeneration extends Page {
     open() {
         super.open('#/min-max-value-generation');
     }
+
+    getDataSetOption() {
+        return browser.element('select[class^=MinMaxValueGeneration__select]').elements('<option>').value[3];
+    }
+
+    getOrgUnitTreeExpandArrowFromTree() {
+        return browser.element('.tree-view').elements('.arrow').value[1];
+    }
+
+    getOneOrgUnitTreeFromTree(index) {
+        return browser.element('.tree-view').elements('div[role=button]').value[index].element('<input>');
+    }
+
+    countSelectedOrgUnit() {
+        return browser.element('.tree-view').elements('div[style*="color: orange"]').value.length;
+    }
 }
 
 module.exports = new MinMaxValueGeneration();
--- e2e/step-definitions/minMaxValueGeneration.steps.js
+++ e2e/step-definitions/minMaxValueGeneration.steps.js
@@ -1,46 +1,68 @@
-const {expect} = require('chai');
-const {defineSupportCode} = require('cucumber');
+const { expect } = require('chai');
+const { defineSupportCode } = require('cucumber');
 
 const minMaxValueGeneration = require('../pages/minMaxValueGeneration.page');
 
-defineSupportCode(({Given, When, Then}) => {
+defineSupportCode(( {Given, When, Then} ) => {
+    // *********************************************************
+    // Background:
+    // *********************************************************
+    // Shared:
+    // that I am logged in to the Sierra Leone DHIS2
     When(/^I open min max value generation page$/, () => {
         minMaxValueGeneration.open();
+        browser.pause(2000);
     });
 
-    Then(/^A column with list of Data Set is displayed$/, () => {
-
+    // *********************************************************
+    // Commons:
+    // *********************************************************
+    // Generate, Remove
+    When(/^User selects a data set$/, () => {
+        minMaxValueGeneration.getDataSetOption().click();
     });
 
-    Then(/^A column with Organization Unit Selection checkboxes$/, () => {
-
+    Then(/^User expands an organization unit selection$/, () => {
+        minMaxValueGeneration.getOrgUnitTreeExpandArrowFromTree().click();
     });
 
-    Then(/^Generate button$/, () => {
-
+    Then(/^User can only select one organization unit$/, () => {
+        minMaxValueGeneration.getOneOrgUnitTreeFromTree(1).click();
+        minMaxValueGeneration.getOneOrgUnitTreeFromTree(3).click();
+        expect(minMaxValueGeneration.countSelectedOrgUnit()).to.equal(1);
     });
 
-    Then(/^Remove button$/, () => {
 
+    // *********************************************************
+    // Scenario: I want to see all items in the page
+    // *********************************************************
+    Then(/^A column with a list of data sets is displayed$/, () => {
+        browser.element('select[class^=MinMaxValueGeneration__select]').isVisible();
     });
 
-    When(/^User selects Data Set$/, () => {
-
+    Then(/^A column with organization unit tree is displayed$/, () => {
+        browser.element('.tree-view').isVisible();
     });
 
-    When(/^User expands Organization Unit Selection$/, () => {
-
+    Then(/^Generate action is displayed$/, () => {
+        browser.element('button[id=generateMinMaxBtnId').isVisible();
     });
 
-    Then(/^User can select several checkboxes$/, () => {
-
+    Then(/^Remove action is displayed$/, () => {
+        browser.element('button[id=removeMinMaxBtnId').isVisible();
     });
 
-    Then(/^User can click in Generate$/, () => {
-
+    // *********************************************************
+    // Scenario: I want to generate min-max value
+    // *********************************************************
+    Then(/^User can click in generate$/, () => {
+        browser.element('button[id=generateMinMaxBtnId').click();
     });
 
-    Then(/^User can click in Remove$/, () => {
-
+    // *********************************************************
+    // Scenario: I want to remove min-max value
+    // *********************************************************
+    Then(/^User can click in remove$/, () => {
+        browser.element('button[id=removeMinMaxBtnId').click();
     });
 });
