85ebc60e5363afade6d31ce7af4fa18ece259187
ismay <ismay@ismaywolff.nl>
2019-05-28 15:49:57 +0200

    chore: use .env files for settings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import { getManifest, getUserSettings } from 'd2/lib/d2';
+import { getUserSettings } from 'd2/lib/d2';
 import D2UIApp from 'd2-ui/lib/app/D2UIApp';
 import log from 'loglevel';
 import { configI18n } from './configI18n';
@@ -10,42 +10,48 @@ import appTheme from './theme';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-log.setLevel(process.env.NODE_ENV === 'production' ? log.levels.INFO : log.levels.DEBUG);
+const { NODE_ENV, REACT_APP_DHIS2_BASE_URL } = process.env
+const isProduction = NODE_ENV === 'production'
 
-const configurations = (userSettings) => {
-    const uiLocale = userSettings.keyUiLocale;
-    sessionStorage.setItem('uiLocale', uiLocale || 'en');
+/**
+ * Configure logging
+ */
+
+log.setLevel(isProduction ? log.levels.INFO : log.levels.DEBUG);
+
+/**
+ * Render app
+ */
+
+ReactDOM.render(
+    <D2UIApp
+        muiTheme={appTheme}
+        initConfig={{
+            baseUrl: `${REACT_APP_DHIS2_BASE_URL}/api`,
+            schemas: [
+                'organisationUnitLevel',
+                'organisationUnitGroup',
+                'organisationUnit',
+                'dataSet',
+            ],
+        }}
+    >
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </D2UIApp>,
+    document.getElementById('app'),
+);
+
+getUserSettings().then((userSettings) => {
+    const { keyUiLocale } = userSettings
+    sessionStorage.setItem('uiLocale', keyUiLocale || 'en');
 
     configI18n(userSettings);
-};
+});
 
-// init d2
-getManifest('manifest.webapp').then((manifest) => {
-    const api = process.env.REACT_APP_DHIS2_API_VERSION ? `/${process.env.REACT_APP_DHIS2_API_VERSION}` : '/';
-    const baseUrl =
-        process.env.NODE_ENV === 'production'
-            ? `${manifest.getBaseUrl()}/api/${manifest.dhis2.apiVersion}`
-            : `${process.env.REACT_APP_DHIS2_BASE_URL}/api${api}`;
-
-    ReactDOM.render(
-        <D2UIApp
-            muiTheme={appTheme}
-            initConfig={{
-                baseUrl,
-                schemas: [
-                    'organisationUnitLevel',
-                    'organisationUnitGroup',
-                    'organisationUnit',
-                    'dataSet',
-                ],
-            }}
-        >
-            <HashRouter>
-                <App />
-            </HashRouter>
-        </D2UIApp>,
-        document.getElementById('app'),
-    );
-}).then(getUserSettings).then(configurations);
+/**
+ * Service worker
+ */
 
 registerServiceWorker();
