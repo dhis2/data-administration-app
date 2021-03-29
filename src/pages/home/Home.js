f4a50389c8ef42135f418a3e597b71d8641b5af3
Médi-Rémi Hashim <mediremi@gmail.com>
2021-03-29 14:47:38 +0100

    fix: restore grid layout

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { sections } from '../sections.conf'
 import GridSection from './grid-section/GridSection'
 import styles from './Home.module.css'
+import './grid.css'
 
 const Home = () => {
     const gridElements = sections.map(section => (
