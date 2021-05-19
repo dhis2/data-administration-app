ac4fe98619300a74d55574debfac34c089c355c3
Médi-Rémi Hashim <mediremi@gmail.com>
2021-05-19 18:23:05 +0100

    feat: test data integrity page

--- cypress/integration/data-integrity/index.js
+++ cypress/integration/data-integrity/index.js
@@ -1,16 +1,44 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Before, Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+
+const JOB_ID = 'JOB_ID'
+
+Before(() => {
+    cy.intercept({ pathname: /dataIntegrity$/, method: 'POST' }, req => {
+        req.reply(200, {
+            response: {
+                id: JOB_ID,
+            },
+        })
+    })
+
+    cy.intercept(
+        { pathname: /system\/taskSummaries\/DATA_INTEGRITY/, method: 'GET' },
+        req => {
+            expect(req.url.endsWith(JOB_ID)).to.be.true
+            req.reply(200, {
+                errors: ['Element 1', 'Element 2', 'Element 3'],
+                errorsWithSubheaders: {
+                    subheader: ['Value 1', 'Value 2'],
+                },
+                passed: {},
+            })
+        }
+    ).as('pollDataIntegrityCheck')
+})
 
 Given('the user navigated to the data integrity page', () => {
+    cy.clock()
     cy.visit('/#/data-integrity')
     cy.findByRole('heading', { name: 'Data Integrity' }).should('exist')
 })
 
 When(`the user clicks on the 'Run integrity checks' button`, () => {
-    // TODO
+    cy.findByRole('button', { name: 'Run integrity checks' }).click()
 })
 
 Then('a data integrity check is requested', () => {
-    // TODO
+    cy.tick(5000)
+    cy.wait('@pollDataIntegrityCheck')
 })
 
 Then('the user is shown the results of the check', () => {
