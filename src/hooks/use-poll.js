3922b0d3623750eb3a983ddaad05ad4e1b621b71
Médi-Rémi Hashim <mediremi@gmail.com>
2021-06-10 12:40:13 +0100

    fix: cancel previous polls when starting poll

--- src/hooks/use-poll.js
+++ src/hooks/use-poll.js
@@ -3,13 +3,14 @@ import { useRef, useEffect } from 'react'
 export const usePoll = (fn, pollInterval) => {
     const intervalId = useRef(null)
 
-    const start = (...args) => {
-        intervalId.current = setInterval(() => fn(...args), pollInterval)
-    }
     const cancel = () => {
         if (intervalId.current) {
             clearInterval(intervalId.current)
         }
+    }
+    const start = (...args) => {
+        cancel()
+        intervalId.current = setInterval(() => fn(...args), pollInterval)
     }
 
     useEffect(() => {
