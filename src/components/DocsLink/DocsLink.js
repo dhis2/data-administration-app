4d0731258e5f1b701338ec77054912a856821c90
Médi-Rémi Hashim <mediremi@gmail.com>
2021-08-05 17:14:05 +0100

    refactor(docs-link): replace material-ui IconButton with Tooltip

--- src/components/DocsLink/DocsLink.js
+++ src/components/DocsLink/DocsLink.js
@@ -1,9 +1,10 @@
 import { useConfig } from '@dhis2/app-runtime'
-import { IconButton } from 'material-ui'
+import { colors, Tooltip, IconQuestion24 } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
 import i18n from '../../locales'
 import { getDocsKeyForSection } from '../../pages/sections.conf'
+import styles from './DocsLink.module.css'
 
 const getDocsVersion = ({ major, minor, tag }) => {
     if (tag === 'SNAPSHOT') {
@@ -18,18 +19,21 @@ const DocsLink = ({ sectionKey }) => {
     const docsKey = getDocsKeyForSection(sectionKey)
 
     return (
-        <IconButton
-            className="helper-icon"
-            iconStyle={{ color: '#276696' }}
-            href={`https://docs.dhis2.org/en/use/user-guides/dhis-core-version-${docsVersion}/maintaining-the-system/data-administration.html#${docsKey}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            tooltip={i18n.t('Open user guide')}
-            tooltipPosition="bottom-center"
-            iconClassName="material-icons"
+        <Tooltip
+            content={i18n.t('Open user guide')}
+            placement="bottom"
+            openDelay={0}
+            closeDelay={0}
         >
-            help
-        </IconButton>
+            <a
+                className={styles.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://docs.dhis2.org/en/use/user-guides/dhis-core-version-${docsVersion}/maintaining-the-system/data-administration.html#${docsKey}`}
+            >
+                <IconQuestion24 color={colors.blue700} />
+            </a>
+        </Tooltip>
     )
 }
 
