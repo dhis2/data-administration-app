d8302ba95aa856027d038fc322c4bd12fea2a3ba
Médi-Rémi Hashim <mediremi@gmail.com>
2021-08-31 17:11:11 +0100

    test: update min-max integration tests

--- cypress/integration/min-max-value-generation/index.js
+++ cypress/integration/min-max-value-generation/index.js
@@ -1,4 +1,4 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps'
+import { Given, When } from 'cypress-cucumber-preprocessor/steps'
 import './user-generates-min-max-values'
 import './user-deletes-min-max-values'
 
@@ -7,4 +7,17 @@ Given('the user navigated to the min-max values page', () => {
     cy.findByRole('heading', { name: 'Min-Max Value Generation' }).should(
         'exist'
     )
+})
+
+When('the user selects a dataset', () => {
+    cy.getWithDataTest('{dhis2-uicore-select-input}').click()
+    cy.findByRole('checkbox', { name: 'ART monthly summary' }).click({
+        force: true,
+    })
+    // Close multiselect
+    cy.get('body').click(0, 0)
+})
+
+When('the user selects an organisation unit', () => {
+    cy.getWithDataTest('{dhis2-uiwidgets-orgunittree-node-label}').click()
 })
--- cypress/integration/min-max-value-generation/user-deletes-min-max-values.js
+++ cypress/integration/min-max-value-generation/user-deletes-min-max-values.js
@@ -14,17 +14,6 @@ Before(() => {
     ).as('deleteMinMaxValues')
 })
 
-When('the user selects a dataset', () => {
-    // Both lines are needed in order for the <select multiple> element to
-    // register the selection
-    cy.get('select').select('ART monthly summary')
-    cy.findByRole('option', { name: 'ART monthly summary' }).click()
-})
-
-When('the user selects an organisation unit', () => {
-    cy.getWithDataTest('{dhis2-uiwidgets-orgunittree-node-label}').click()
-})
-
 When(`the user clicks the 'Remove min-max values' button`, () => {
     cy.findByRole('button', { name: 'Remove min-max values' }).click()
 })
