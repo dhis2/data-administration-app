c1a69cf1b73d22ae6265a668ba552abbb9304c8b
Médi-Rémi Hashim <mediremi@gmail.com>
2022-02-06 19:16:40 +0000

    fix(data-statistics): sort objects by label

--- src/pages/data-statistics/parse-tables.js
+++ src/pages/data-statistics/parse-tables.js
@@ -52,12 +52,17 @@ const objectCountsTableFromResponse = objectCountsResponse => {
         return null
     }
 
-    return {
-        label: i18n.t('Object type'),
-        elements: Object.entries(objectCountsResponse).map(([key, count]) => ({
+    const elements = Object.entries(objectCountsResponse).map(
+        ([key, count]) => ({
             label: objectLabels[key] || key,
             count,
-        })),
+        })
+    )
+    elements.sort((a, b) => a.label.localeCompare(b.label))
+
+    return {
+        label: i18n.t('Object type'),
+        elements,
     }
 }
 
