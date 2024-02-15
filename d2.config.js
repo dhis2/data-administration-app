70443aca48eabca45025e8e76706bda287e11725
Birk Johansson <birk@dhis2.org>
2024-02-15 16:02:11 +0100

    chore: add AppHub id to d2.config

--- d2.config.js
+++ d2.config.js
@@ -1,9 +1,9 @@
 const config = {
+    id: 'd685d128-3b31-4a21-adbf-bd1487c3004c',
     type: 'app',
     name: 'data-administration',
     title: 'Data Administration',
     coreApp: true,
-
     minDHIS2Version: '2.39',
     entryPoints: {
         app: './src/App.js',
