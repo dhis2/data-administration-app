ed576e8ac205281fc2d143b6fd04cd092bd293cb
drita <drnanyanzi@gmail.com>
2025-09-11 23:50:34 +0200

    feat: add shortcuts to command palette

--- d2.config.js
+++ d2.config.js
@@ -1,3 +1,4 @@
+/** @type {import('@dhis2/cli-app-scripts').D2Config} */
 const config = {
     id: 'd685d128-3b31-4a21-adbf-bd1487c3004c',
     type: 'app',
@@ -8,6 +9,36 @@ const config = {
     entryPoints: {
         app: './src/App.jsx',
     },
+    shortcuts: [
+        {
+            name: 'Data and metadata integrity',
+            url: '#/data-integrity',
+        },
+        {
+            name: 'Data and metadata maintenance',
+            url: '#/maintenance',
+        },
+        {
+            name: 'Resource tables',
+            url: '#/resourceTables',
+        },
+        {
+            name: 'Analytics tables',
+            url: '#/analytics',
+        },
+        {
+            name: 'Data statistics',
+            url: '#/data-statistics',
+        },
+        {
+            name: 'Lock exception',
+            url: '#/lock-exceptions',
+        },
+        {
+            name: 'Min/Max value generation',
+            url: '#/min-max-value-generation',
+        },
+    ],
 }
 
 module.exports = config
